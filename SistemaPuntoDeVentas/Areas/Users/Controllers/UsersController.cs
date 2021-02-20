6639cfdf5d45efec537cab8a26f075e06be7a129
Carlos Roberto <Carlos.Rodriguez3@Chubb.com>
2021-02-20 12:31:59 -0600

    Agregando el proyecto

---
+++ SistemaPuntoDeVentas/Areas/Users/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaPuntoDeVentas.Areas.Users.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        [Area("Users")]
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaPuntoDeVentas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaPuntoDeVentas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
